ff3f456fe36a4193582937a2fc2bb97780eec815
Nathan <nathanbailie.dev@gmail.com>
2023-03-10 16:09:37 +0300

    create readme.md

---
+++ README.md
@@ -0,0 +1,15 @@
+# WordPress
+
+## Description
+This is a one page web site about a WordPress seminar
+
+## Demo
+<img src="https://github.com/NathanBailie/WordPress/raw/main/WordPress.gif" width="600" />
+
+<img src="https://github.com/NathanBailie/WordPress/raw/main/WordPress(adaptive).gif" width="600" />
+
+## Tech
+* HTML
+* CSS (SCSS, flex, grid, cross-browser adaptive)
+* JavaScript
+* GULP
--- dist/js/script.js
+++ dist/js/script.js
@@ -3,19 +3,17 @@
 let upChevron = document.querySelector('.up-chevron');
 let clientHeight = document.documentElement.clientHeight;
 
-
 upChevron.addEventListener('click', function () {
 	window.scrollTo({
 		top: 0,
 		behavior: "smooth"
 	});
-
-})
+});
 
 window.addEventListener('scroll', function () {
 	if (document.documentElement.scrollTop > clientHeight) {
 		upChevron.style.display = 'flex';
 	} else {
 		upChevron.style.display = 'none';
-	}
+	};
 });
